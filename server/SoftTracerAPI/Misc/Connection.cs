0cb9b9931353fb4fafab08bcae9da3bd1ff170c2
Jean Sanson <jean.sanson@movtech.inf.br>
2020-10-03 21:18:37 -0300

    Implementa conexão

--- server/SoftTracerAPI/Misc/Connection.cs
+++ server/SoftTracerAPI/Misc/Connection.cs
@@ -1,11 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Data.SqlClient;
+
 
 namespace SofTracerAPI.Misc
 {
     public class SoftTracerConnection
     {
+        public readonly SqlConnection connection = new SqlConnection("Server = root@localhost:4000; Database=softracer; Uid=movtech;Pwd=mvt;MultipleActiveResultSets=True");
+
+        public SoftTracerConnection()
+        {
+            connection.Open();
+        }
+
+        public SqlCommand GetCommand()
+        {
+            SqlCommand command = connection.CreateCommand();
+            command.Connection = connection;
+            return command;
+
+        }
+
     }
 }
