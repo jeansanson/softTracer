a614c6a6979baa46001c169e7a9a0eed5b52fbad
Bruno Klehm <brunowuklehm@gmail.com>
2020-11-01 22:25:07 -0300

    Início do front-end

---
+++ client/softtracer/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+      elevation="1"
+    >
+    
+      <h1>SoftTracer</h1>
+
+      <v-spacer></v-spacer>
+
+      <!-- TODO: Aqui irá ficar o botão de sair, apenas caso o usuário estiver logado -->
+    </v-app-bar>
+
+    <v-main>
+      <Login/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Login from './components/Login';
+
+export default {
+  name: 'App',
+
+  components: {
+    Login,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ client/softtracer/src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12" sm="4"></v-col>
+      <v-col cols="12" sm="4">
+        <h1 class="display-1 font-weight-bold mb-3">
+          Login
+        </h1>
+
+        <v-text-field label="Usuário"></v-text-field>
+        <v-text-field
+          label="Senha"
+          :type="show1 ? 'text' : 'password'"
+        ></v-text-field>
+        <v-btn elevation="2" color="primary">Entrar</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Login",
+
+  data: () => ({}),
+};
+</script>
