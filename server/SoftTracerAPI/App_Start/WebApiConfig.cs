d35a0b372f24dcfaba2f9e4fa7fd5494354d4503
Jean Sanson <jean.sanson@movtech.inf.br>
2020-11-29 13:13:17 -0300

    correção enable cors

--- server/SoftTracerAPI/App_Start/WebApiConfig.cs
+++ server/SoftTracerAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace SoftTracerAPI
 {
@@ -9,11 +10,10 @@ namespace SoftTracerAPI
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-            config.EnableCors();
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
